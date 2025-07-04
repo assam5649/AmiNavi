8384624e20b2b4ce6f21a7a4278b2235f20ecb0d
Shutaro Kawamitsu <assam5649.18@gmail.com>
2025-07-05 08:16:59 +0900

    revert: restore temporarily removed models package
    Although this package is planned for removal, it is currently required by some Go files.
    Cleanup will be handled in a separate issue.

---
+++ server/internal/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type User struct {
+	ID              int       `json:"id"`
+	FirebaseUID     string    `json:"firebase_uid"`
+	LoginID         string    `json:"login_id"`
+	DisplayName     string    `json:"display_name"`
+	ProfileImageURL string    `json:"profile_image_url"`
+	EMail           string    `json:"email"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
