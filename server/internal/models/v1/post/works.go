95e8377c4b7b71efe21e48b8d6b1dbb977600a1e
Shutaro Kawamitsu <assam5649.18@gmail.com>
2025-07-19 17:08:27 +0900

    feat: initial DTO definitions

--- server/internal/models/v1/post/works.go
+++
@@ -1,8 +0,0 @@
-package post
-
-type Works struct {
-	Title	string	`json:"title"`,
-	WorkURL	string	`json:"work_url"`,
-	Count	int	`json:"count"`,
-	Bookmark	bool	`json:"bookmark"`
-}
--- server/internal/models/v1/put/works_id.go
+++
@@ -1,8 +0,0 @@
-package put
-
-type WorksId struct {
-	Title	string	`json:"title"`,
-	WorkURL	string	`json:"work_url"`,
-	Count	int	`json:"count"`,
-	Bookmark	string	`json:"bookmark"`
-}
