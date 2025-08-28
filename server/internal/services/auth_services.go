ea8ba2e5b2175e663a3a60b715c2e226d598b84e
Shutaro Kawamitsu <assam5649.18@gmail.com>
2025-08-28 09:34:26 +0900

    refactor: define wrapper AuthServices for dependencies and implement interfaces

--- server/internal/services/auth_services.go
+++ server/internal/services/auth_services.go
@@ -1,18 +1,51 @@
 package services
 
 import (
+	firebase "firebase.google.com/go/v4/auth"
+	"gorm.io/gorm"
 	"server/internal/db"
 	"server/internal/dto/patch"
 	"server/internal/models"
 )
 
-func RegisterIfNotExists(uid string) (*models.User, bool, error) {
-	user, exists, err := db.FindByUID(uid)
+type RegisterService interface {
+	RegisterIfNotExists(uid string) (*models.User, bool, error)
+}
+
+type UpdateService interface {
+	Update(uid string, request *patch.UpdateRequest) (*models.User, error)
+}
+
+type RegisterServiceImpl struct {
+	DB *gorm.DB
+}
+type UpdateServiceImpl struct {
+	DB *gorm.DB
+}
+
+type AuthServices struct {
+	DB           *gorm.DB
+	FirebaseAuth *firebase.Client
+	Register     RegisterService
+	Update       UpdateService
+}
+
+func NewAuthService(db *gorm.DB, firebaseAuthClient *firebase.Client, register RegisterService, update UpdateService) *AuthServices {
+	return &AuthServices{
+		DB:           db,
+		FirebaseAuth: firebaseAuthClient,
+		Register:     register,
+		Update:       update,
+	}
+}
+
+func (s *RegisterServiceImpl) RegisterIfNotExists(uid string) (*models.User, bool, error) {
+	user, exists, err := db.FindByUID(s.DB, uid)
 	if exists {
 		return user, exists, err
 	}
 
-	registerModel, err := db.CreateUser(uid)
+	registerModel, err := db.CreateUser(s.DB, uid)
 	if err != nil {
 		return nil, exists, err
 	}
@@ -20,12 +53,12 @@ func RegisterIfNotExists(uid string) (*models.User, bool, error) {
 	return registerModel, exists, nil
 }
 
-func Update(uid string, request *patch.UpdateRequest) (*models.User, error) {
+func (s *UpdateServiceImpl) Update(uid string, request *patch.UpdateRequest) (*models.User, error) {
 	var user models.User
 	user.DisplayName = request.DisplayName
 	user.ProfileImageURL = request.ProfileImageURL
 
-	if err := db.Update(uid, &user); err != nil {
+	if err := db.Update(s.DB, uid, &user); err != nil {
 		return nil, err
 	}
 
