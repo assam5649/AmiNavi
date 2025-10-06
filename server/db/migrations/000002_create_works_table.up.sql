987a69942e2a696b0ef6c0e2863e6ca995f528a2
assam <assam5649.18@gmail.com>
2025-10-06 16:13:29 +0900

    fix: Add indices to users and works for SQL performance improvement

--- server/db/migrations/000002_create_works_table.up.sql
+++ server/db/migrations/000002_create_works_table.up.sql
@@ -1,3 +1,5 @@
+CREATE INDEX idx_users_firebase_uid ON users (firebase_uid);
+
 CREATE TABLE IF NOT EXISTS works (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     author VARCHAR(256)
@@ -14,6 +16,9 @@ CREATE TABLE IF NOT EXISTS works (
     completed_at DATETIME,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    INDEX idx_works_author (author),
+
     FOREIGN KEY (author) REFERENCES users(firebase_uid)
         ON UPDATE CASCADE
         ON DELETE CASCADE
